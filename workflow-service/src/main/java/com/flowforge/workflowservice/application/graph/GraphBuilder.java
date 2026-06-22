998899221868fe2cce17842ca22a358dd1fe22f4
Anzyll <anzil2003m@gmail.com>
2026-06-22 11:50:07 +0530

    feat: add graph builder for workflow DAG

---
+++ workflow-service/src/main/java/com/flowforge/workflowservice/application/graph/GraphBuilder.java
@@ -0,0 +1,28 @@
+package com.flowforge.workflowservice.application.graph;
+
+import com.flowforge.workflowservice.domain.edge.WorkflowEdge;
+import com.flowforge.workflowservice.domain.node.WorkflowNode;
+import org.springframework.stereotype.Component;
+import java.util.*;
+
+@Component
+public class GraphBuilder {
+    public Map<UUID, List<UUID>> buildGraph(
+            List<WorkflowNode> nodes,
+            List<WorkflowEdge> edges){
+
+        Map<UUID,List<UUID>> graph = new HashMap<>();
+
+        for(WorkflowNode node : nodes){
+            graph.put(node.getId(),new ArrayList<>());
+        }
+
+        for(WorkflowEdge edge : edges){
+            UUID sourceId = edge.getSourceNode().getId();
+            UUID targetid = edge.getTargetNode().getId();
+
+            graph.get(sourceId).add(targetid);
+        }
+        return graph;
+    }
+}
