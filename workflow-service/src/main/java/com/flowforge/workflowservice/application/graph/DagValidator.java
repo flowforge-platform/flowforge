fc54997e23263f8b101e84fdbcdc82c3f6b47e26
Anzyll <anzil2003m@gmail.com>
2026-06-22 12:39:48 +0530

    feat: add DFS-based DAG cycle detection

---
+++ workflow-service/src/main/java/com/flowforge/workflowservice/application/graph/DagValidator.java
@@ -0,0 +1,40 @@
+package com.flowforge.workflowservice.application.graph;
+
+import org.springframework.stereotype.Component;
+
+import java.util.*;
+
+@Component
+public class DagValidator {
+    public boolean hasCycle(Map<UUID, List<UUID>> graph){
+        Set<UUID> visited = new HashSet<>();
+        Set<UUID> visiting = new HashSet<>();
+
+        for(UUID node : graph.keySet()){
+            if(dfs(node,graph,visiting,visited)){
+                return  true;
+            }
+        }
+        return false;
+    }
+
+    private boolean dfs(UUID node,Map<UUID,List<UUID>> graph,Set<UUID> visiting,Set<UUID> visited){
+        if (visiting.contains(node)){
+            return  true;
+        }
+        if (visited.contains(node)){
+            return  false;
+        }
+
+        visiting.add(node);
+
+        for(UUID neighbour : graph.get(node)){
+            if (dfs(neighbour,graph,visiting,visited)) {
+                return true;
+            }
+        }
+        visiting.remove(node);
+        visited.add(node);
+        return false;
+    }
+}
