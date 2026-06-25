c1f0c1c55f65d340e6b7bcb0d421bf20f838b11f
Anzyll <anzil2003m@gmail.com>
2026-06-25 11:54:52 +0530

    implement retriving workflow execution apis

--- workflow-service/src/main/java/com/flowforge/workflowservice/infrastructure/persistence/WorkflowExecutionRepository.java
+++ workflow-service/src/main/java/com/flowforge/workflowservice/infrastructure/persistence/WorkflowExecutionRepository.java
@@ -4,8 +4,18 @@ import com.flowforge.workflowservice.domain.execution.WorkflowExecution;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+import java.util.Optional;
 import java.util.UUID;
 
 @Repository
 public interface WorkflowExecutionRepository extends JpaRepository<WorkflowExecution, UUID> {
+    Optional<WorkflowExecution> findByIdAndOrganizationId(
+            UUID id,
+            UUID organizationId
+    );
+
+    List<WorkflowExecution> findAllByOrganizationIdOrderByStartedAtDesc(
+            UUID organizationId
+    );
 }
