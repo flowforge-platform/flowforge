4fff445354d90ec90f62e8aa6f08b02df2dc4f07
axwinachu <ceaswin146@gmail.com>
2026-07-08 13:10:32 +0530

    added migration of worker-service

--- worker-service/src/main/resources/db/migration/V2__add_worker_task_execution_columns.sql
+++
@@ -1,6 +0,0 @@
-ALTER TABLE worker_tasks
-    ADD COLUMN IF NOT EXISTS workflow_execution_id VARCHAR(36),
-    ADD COLUMN IF NOT EXISTS node_id VARCHAR(36);
-
-ALTER TABLE worker_tasks
-    ALTER COLUMN payload DROP NOT NULL;
