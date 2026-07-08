aa3ad52144aac5e407b88e059af1c4fd06a4c8d3
axwinachu <ceaswin146@gmail.com>
2026-07-08 10:41:04 +0530

    fix id string to uuid

--- auth-service/src/main/java/com/flowforge/auth_service/model/User.java
+++ auth-service/src/main/java/com/flowforge/auth_service/model/User.java
@@ -10,6 +10,8 @@ import org.hibernate.annotations.CreationTimestamp;
 
 import java.time.Instant;
 
+import java.util.UUID;
+
 @Entity
 @Table(name = "users")
 @Data
@@ -20,7 +22,7 @@ public class User {
 
     @Id
     @GeneratedValue(strategy = GenerationType.UUID)
-    private String id;
+    private UUID id;
 
     @Column(nullable = false)
     private String fullName;
