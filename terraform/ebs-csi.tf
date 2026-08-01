e7314357f3e1aa15768dd36128f16d1ce370094d
Anzyll <anzil2003m@gmail.com>
2026-08-01 20:27:09 +0530

    Deploy FlowForge on AWS EKS with ALB Ingress

---
+++ terraform/ebs-csi.tf
@@ -0,0 +1,19 @@
+data "aws_iam_policy" "ebs_csi" {
+  arn = "arn:aws:iam::aws:policy/service-role/AmazonEBSCSIDriverPolicy"
+}
+
+module "ebs_csi_irsa_role" {
+  source  = "terraform-aws-modules/iam/aws//modules/iam-role-for-service-accounts-eks"
+  version = "~> 5.50"
+
+  role_name = "flowforge-ebs-csi"
+
+  attach_ebs_csi_policy = true
+
+  oidc_providers = {
+    main = {
+      provider_arn               = module.eks.oidc_provider_arn
+      namespace_service_accounts = ["kube-system:ebs-csi-controller-sa"]
+    }
+  }
+}
---
+++ terraform/variables.tf
@@ -0,0 +1,11 @@
+variable "aws_region" {
+  type = string
+}
+
+variable "cluster_name" {
+  type = string
+}
+
+variable "cluster_version" {
+  type = string
+}
